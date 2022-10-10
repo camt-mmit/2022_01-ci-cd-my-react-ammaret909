707023575f3e9c1fd78d411cf98942bb3cb8e606
Kaw <ammaret_aituphon@cmu.ac.th>
2022-10-10 11:55:33 +0700

    new

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Test feature-000123
+          Learn React Test feature-000123456
         </a>
       </header>
     </div>
